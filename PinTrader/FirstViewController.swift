48a8a7c874eb7c2df3d95b0109d88c1e804b2ebc
Federico Trimboli <federicotrimboli@gmail.com>
2018-05-28 22:34:39 -0300

    Add template app

---
+++ PinTrader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PinTrader
+//
+//  Created by Federico Trimboli on 28/05/2018.
+//  Copyright © 2018 Federico Trimboli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
